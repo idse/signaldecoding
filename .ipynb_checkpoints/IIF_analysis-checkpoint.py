d400f2c7ac31d797716283ee585ee0453d4af80e
Idse Heemskerk <idse.heemskerk@gmail.com>
2025-08-11 15:35:07 -0400

    moved some class definitions to .py file

--- .ipynb_checkpoints/IIF_analysis-checkpoint.py
+++ .ipynb_checkpoints/IIF_analysis-checkpoint.py
@@ -1,5 +1,147 @@
+class Metadata: 
+
+    def __init__(self):
+
+        self.xres = np.nan;
+        self.yres = np.nan;
+        self.channels = [];
+        self.conditions = [];
+
+    def conditionStartPos(self, condition):
+        # provide the first position for absa given condition
+        print('TODO: implement')
+
+    @property
+    def nChannels(self):
+        return len(meta.channels)
+        
+
 class Position:
     
-    def __init__(cellData)
+    def __init__(self, data, posID, meta, features):
 
-        self.cellData = cellData
+        self.cellData = dict();
+        
+        self.nCells = data.shape[0]
+        self.condition = data['condition'].iloc[0]; # called well in matlab
+        
+        self.cellData['XY'] = data[['X','Y']];
+        self.cellData['features'] = data[feature_names];
+        self.cellData['intensities'] = data[meta.channels];
+        
+        self.ID = posID
+
+    def scatter(self, channel, ms=1, vmin=0, vmax=3):
+        # make a scatter plot of the colony
+        # 
+        # channel: color channel 
+        # ms : scatter point size
+        # vmin, vmax : min and max color
+        
+        color = self.cellData['intensities'][channel]
+        order = color.sort_values().index;
+        
+        plt.scatter(self.cellData['XY']['X'][order], self.cellData['XY']['Y'][order], s=ms, c=color[order], cmap='YlGnBu', vmin=vmin, vmax=vmax)
+        plt.axis('square');
+        plt.axis('off');
+        
+class Colony(Position):
+    # Colony extends Position to include features and methods specific to disc-shaped micropatterned colonies, like radiusMicron and makeRadialProfile(..)
+
+    def __init__(self, data, posID, meta, features, nominalRadius):
+
+        super().__init__(data, posID, meta, features)
+        
+        self.radiusMicron = nominalRadius
+        self.radiusPixel = nominalRadius/meta.xres
+        self.center = data[['X','Y']].mean(); # could further clean up by excluding cells outside the colony as in matlab
+        self.cellData['XY'] = self.cellData['XY'].assign(edgeDist=data['MetricDist']);
+        self.calcRadialProfiles()
+        self.calcPosError()
+        
+    def calcRadialProfiles(self, cellsPerBin=100, overlap=50, dr=10):
+        # overlap is not really necessary if we do a Gaussian smoothing afterwards, which works better
+        # dr : spacing of regular radial grid on which interpolation is performed
+        
+        # first create bins with equal numbers of cells and calculate the mean edgeDist & mean,std intensity in those bins
+        Nbins = round(self.nCells/cellsPerBin);
+        edges = np.linspace(0,  self.nCells-1, Nbins+1).round().astype(int);
+
+        Rs = self.cellData['XY']['edgeDist'];
+        I = Rs.sort_values().index;
+        
+        r_tmp = np.zeros(Nbins)
+        profile_tmp = pd.DataFrame(columns=self.cellData['intensities'].columns, index=range(Nbins))
+        profile_tmp_std = pd.DataFrame(columns=self.cellData['intensities'].columns, index=range(Nbins))
+        
+        for i in range(Nbins):
+            
+            start = max(edges[0], edges[i]-overlap)
+            stop = min(edges[i+1]+overlap, edges[-1])
+            ptidx = np.arange(start, stop+1) 
+            
+            sel = I[ptidx]
+            r_tmp[i] = np.mean(Rs[sel])
+            profile_tmp.loc[i, :] = np.nanmean(self.cellData['intensities'].loc[sel, :], axis=0)
+            profile_tmp_std.loc[i, :] = np.nanstd(self.cellData['intensities'].loc[sel, :], axis=0)
+
+        r_tmp[-1] = max(Rs); # for center bin, the r value should be the center, not the average r value of the points in it
+        
+        # now interpolate on evenly spaced radial bins that allow easy averaging between colonies
+        margin = 0;
+        maxR = self.radiusMicron + margin
+        # I am adding 10% negative R values to the grid on which it will linearly extrapolate, to deal with boundary effects for smoothing on the edge 
+        # (positional error goes high on edge because mirroring the data for smoothing makes the gradient zero there)
+        Ngrid = round(1.1*maxR/dr) 
+        self.radialGrid = np.linspace(-0.1*maxR, maxR, Ngrid);
+        
+        self.radialProfiles = pd.DataFrame(columns=self.cellData['intensities'].columns, index=range(Ngrid))
+        self.radialProfiles_std = pd.DataFrame(columns=self.cellData['intensities'].columns, index=range(Ngrid))
+        
+        for channel in meta.channels:
+            
+            # if you get an error from interpolate saying invalid "invalid value encountered in divide", it is because the same r_tmp value occurs twice because cellPerBin < 2* number of cells with edgeDist 0
+            interpolator = interp.interp1d(r_tmp, profile_tmp[channel], kind='linear', fill_value='extrapolate', bounds_error=False)
+            self.radialProfiles[channel] = interpolator(self.radialGrid);
+
+            interpolator = interp.interp1d(r_tmp, profile_tmp_std[channel], kind='linear', fill_value='extrapolate', bounds_error=False)
+            self.radialProfiles_std[channel] = interpolator(self.radialGrid);
+
+    def calcPosError(self, sigma=1):
+        # sig: standard deviation (in bins) for Gaussian smoothing of profile and std
+
+        r = self.radialGrid
+        y = self.radialProfiles.apply(lambda col: gaussian_filter1d(col, sigma=sigma))
+        yerr = self.radialProfiles_std.apply(lambda col: gaussian_filter1d(col, sigma=sigma))
+        dy = y.apply(lambda col: np.gradient(col,r))
+        self.posError = yerr/np.abs(dy)*(100/self.radiusMicron);
+
+            
+    def plotRadialProfile(self, channel):
+        # plot radial mean and std for some channel
+        
+        r = self.radialGrid;
+        y = self.radialProfiles[channel];
+        yerr = self.radialProfiles_std[channel];
+        
+        plt.plot(r, y)
+        plt.fill_between(r, y - yerr, y + yerr, alpha=0.3, color='blue', edgecolor='none')
+        
+        plt.gca().set_box_aspect(1)
+        plt.ylabel("intensity")
+        plt.xlabel(r"edge distance ($\mu m$)")
+        plt.xlim((0, self.radiusMicron));
+
+    def plotPosError(self, channel, mode='all'):
+
+        r = self.radialGrid
+        perr = self.posError[channel]
+
+        plt.plot(r, perr);
+        plt.gca().set_box_aspect(1)
+        plt.ylabel('pos error (%)')
+        plt.xlabel(r"edge distance ($\mu m$)")
+        
+        plt.ylim(0,30)
+        plt.xlim(0, self.radiusMicron)
+        
